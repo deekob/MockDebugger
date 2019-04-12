399dccf441f344a3ac2b2a42e5b7ce7b862742a0
deekob <derek.w.bingham@gmail.com>
2019-04-12 10:15:51 +0800

    Initial

---
+++ debugger/Handler.cs
@@ -0,0 +1,19 @@
+using Amazon.Lambda.Core;
+using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Namespace
+{
+    public class Handler
+    {
+       public JObject FunctionHandler(JObject input)
+       {
+            //Todo - write more code dbing :oP
+
+
+           return input;
+       }
+    }
+}
